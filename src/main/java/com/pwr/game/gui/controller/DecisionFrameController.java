58fe17dbb67d24aea29106b5ee16b074a68c0d79
zofia <zofiaanna000@gmail.com>
2018-03-11 15:38:09 +0100

    DecisionFrame, DecisionFrameController added

---
+++ src/main/java/com/pwr/game/gui/controller/DecisionFrameController.java
@@ -0,0 +1,64 @@
+package com.pwr.game.gui.controller;
+
+import com.pwr.game.engine.Game;
+import com.pwr.game.engine.GameImpl;
+import com.pwr.game.gui.view.DecisionFrame;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * @author zlakomiak226190
+ */
+public class DecisionFrameController {
+
+    private final DecisionFrame decisionFrame;
+    private final Game game;
+
+    private final String exitMessage = "Do zobaczenia przy następnej grze!";
+
+    private JButton continueButton;
+    private JButton rankingResetButton;
+    private JButton gameExitButton;
+
+    public DecisionFrameController(DecisionFrame decisionFrame, Game game) {
+        this.decisionFrame = decisionFrame;
+        this.game = game;
+
+        initComponents();
+        initListeners();
+    }
+
+    private void initComponents() {
+        decisionFrame.setVisible(true);
+
+        continueButton = decisionFrame.getContinueButton();
+        rankingResetButton = decisionFrame.getRankingResetButton();
+        gameExitButton = decisionFrame.getGameExitButton();
+
+        decisionFrame.createRankingTable(game);
+        decisionFrame.createCongratulationsLabel();
+        decisionFrame.createButtonsLabels();
+    }
+
+    private void initListeners() {
+        continueButton.addActionListener(al -> {
+            game.newGame();
+            decisionFrame.dispose();
+        });
+
+        rankingResetButton.addActionListener(al -> {
+            game.resetGame();
+            decisionFrame.createRankingTable(game);
+            decisionFrame.createCongratulationsLabel();
+        });
+
+        gameExitButton.addActionListener(al -> {
+            JOptionPane.showMessageDialog(null, exitMessage);
+            System.exit(0);
+        });
+    }
+
+
+}
---
+++ src/main/java/com/pwr/game/gui/view/DecisionFrame.java
@@ -0,0 +1,121 @@
+package com.pwr.game.gui.view;
+
+import com.pwr.game.engine.Game;
+import com.pwr.game.engine.model.Player;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zlakomiak226190
+ */
+public class DecisionFrame extends JFrame {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static final String decisionFrameTitle = "Ranking graczy";
+    private static final String congratulationsString = "Gratulacje";
+    private static final String continueButtonText = "Kontynuuj";
+    private static final String rankingResetButtonText = "Resetuj ranking";
+    private static final String gameExitButtonText = "Zakończ grę";
+
+    private JPanel decisionFramePanel;
+
+    private JPanel congratulationsPanel;
+    private JLabel congratulationsLabel;
+
+    private JPanel buttonsPanel;
+    private JButton continueButton;
+    private JButton rankingResetButton;
+    private JButton gameExitButton;
+
+    private JPanel rankingPanel;
+    private JScrollPane rankingTableScrollPane;
+    private JTable rankingTable;
+    private String[] rankingTableColumnNames = {"Gracz", "Liczba punktów"};
+    private Object[][] rankingTableData;
+
+    public JButton getContinueButton() {
+        return continueButton;
+    }
+
+    public JButton getRankingResetButton() {
+        return rankingResetButton;
+    }
+
+    public JButton getGameExitButton() {
+        return gameExitButton;
+    }
+
+    public JPanel getDecisionFramePanel() {
+        return decisionFramePanel;
+    }
+
+
+    public DecisionFrame() {
+        super(decisionFrameTitle);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setContentPane(decisionFramePanel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void createButtonsLabels() {
+        continueButton.setText(continueButtonText);
+        rankingResetButton.setText(rankingResetButtonText);
+        gameExitButton.setText(gameExitButtonText);
+        buttonsPanel.repaint();
+        buttonsPanel.revalidate();
+    }
+
+    public void createCongratulationsLabel() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(congratulationsString + ": ");
+        int maxPoints = (int) rankingTableData[0][1];
+        stringBuilder.append(rankingTableData[0][0]);
+        int i = 1;
+        while ((int) rankingTableData[i][1] == maxPoints) {
+            stringBuilder.append(", ").append(rankingTableData[i][0]);
+            i++;
+        }
+        stringBuilder.append("!!!");
+        congratulationsLabel.setText(stringBuilder.toString());
+        congratulationsPanel.repaint();
+        congratulationsPanel.revalidate();
+    }
+
+    public void createRankingTable(Game game) {
+        Map<Integer, Integer> points = game.getPoints();
+        rankingTableData = new Object[points.size()][2];
+        Map<Integer, Integer> sortedPoints = MapUtil.sortByValue(points);
+
+        int i = 0;
+        List<Player> players = game.getPlayers();
+        for (Map.Entry<Integer, Integer> entry : sortedPoints.entrySet()) {
+            Player player = players.get(entry.getKey());
+            rankingTableData[i][0] = player.getTurtle();
+            rankingTableData[i][1] = entry.getValue();
+            i++;
+        }
+        rankingTable = new JTable(rankingTableData, rankingTableColumnNames);
+        rankingTableScrollPane.setViewportView(rankingTable);
+        rankingPanel.repaint();
+        rankingPanel.revalidate();
+    }
+}
+
+class MapUtil {
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+}
